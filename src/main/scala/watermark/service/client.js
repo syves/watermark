552dd47466376e6f4158daf50cbf498c184a155d
Shakrah Yves <7shakrahs@gmail.com>
2016-11-13 22:13:33 +0100

    refactored script

--- src/main/scala/watermark/service/client.js
+++ src/main/scala/watermark/service/client.js
@@ -2,8 +2,6 @@
 
 var request = require('request');
 
-// node Client.js
-
 var docs = [
   'book\tCosmos\tCarl Sagan\tScience',
   'journal\tThe Journal of cell biology\tRockefeller University Press',
@@ -19,6 +17,7 @@ var docs = [
   'book\tBlack Holes and Baby Universes\tStephen W Hawking\tScience',
 ];
 
+//Store tickets to represent Clients that have recieved a Ticket
 var tickets = [];
 
 //  watermark :: Document -> Promise Ticket
@@ -41,6 +40,7 @@ var watermark = function(doc) {
 
 //  retrieve :: Ticket -> Promise Document
 var retrieve = function(ticket) {
+  //Q: js promises?
   return new Promise(function(resolve, reject) {
     request({
       method: 'POST',
@@ -56,8 +56,9 @@ var retrieve = function(ticket) {
   });
 };
 
-var loop = function loop() {
-  //Mimic 12 clients requesting documents be watermarked at once.
+//Q: js event loop?
+function simulateWaterMarkRequest() {
+  //Mimic 12 clients requesting documents to be watermarked at once.
   if (docs.length > 0) {
     var doc = docs.shift();
     //Rest API for retieving a ticket for a Document being watermarked in the future.
@@ -66,17 +67,25 @@ var loop = function loop() {
       console.log('ticket created:', ticket);
       console.log('tickets are: ' + tickets);
     });
-    setTimeout(loop, 100 * Math.random());
+    //Simulate request traffic
+    setTimeout(simulateWaterMarkRequest, 100 * Math.random());
   }
-  //If a user has retrieved a ticket, then model the user requesting the
-  //waterMark by ticket.
+}
+
+//If a user has retrieved a ticket, then model the user requesting the waterMark by ticket.
+function simulateRetrieveDocument() {
   if (tickets.length > 0) {
     var ticket = tickets[Math.floor(tickets.length * Math.random())];
     //Rest API for retrieving a Document.
+    //"Then" is equivalent to Future.onComplete, a nicer way of handling side
+    // effects than using callbacks.
     retrieve(ticket).then(function(doc) {
       console.log('doc:' + doc);
+      console.log("");
       //console.log('doc:', JSON.parse(doc));
     });
   }
+  setTimeout(simulateRetrieveDocument, 100 * Math.random());
 }
-loop();
+simulateWaterMarkRequest();
+//simulateRetrieveDocument();
